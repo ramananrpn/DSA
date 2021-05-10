54821e52eceac518acd586ff25696542a04f744c
ramananrpn <ramananrpn@gmail.com>
2021-05-10 13:13:35 +0530

    HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ Leetcode/hashmap/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,61 @@
+package hashmap;
+
+/* https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/ */
+
+/*
+* Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation:
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1).
+For nums[3]=2 there exist one smaller number than it (1).
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+Example 2:
+
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+Example 3:
+
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+
+Constraints:
+
+2 <= nums.length <= 500
+0 <= nums[i] <= 100
+* */
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class HowManyNumbersAreSmallerThanTheCurrentNumber {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+//         Red-Black Tree
+        TreeSet<Integer> treeSet = new TreeSet<>(new MyComparator());
+//         Adding elements
+        for(int n : nums){
+            treeSet.add(n);
+        }
+        for(int a : treeSet){
+            System.out.println(a);
+        }
+
+        for(int i = 0 ;i < nums.length ; i++) {
+            nums[i] = treeSet.headSet(nums[i]).size();
+        }
+        return nums;
+    }
+    class MyComparator implements Comparator<Integer> {
+        public int compare(Integer a, Integer b) {
+            // This will allow duplicate elements to be added to the sorted set.
+            if(a == b) return -1;
+            else return a-b;
+        }
+    }
+}
