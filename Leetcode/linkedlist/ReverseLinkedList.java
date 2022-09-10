deacddc6a0f40c26ee177b9aa1e739fac2a7c213
ramananrpn <ramananrpn@gmail.com>
2022-09-10 23:20:11 +0530

    ReverseLinkedList

---
+++ Leetcode/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,39 @@
+package linkedlist;
+
+// https://leetcode.com/problems/reverse-linked-list/
+
+/*
+* Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+*/
+
+// TAGS: easy, three_pointer
+
+// Iterative 3 pointer approach
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode prev = null;
+
+        while(cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+}
