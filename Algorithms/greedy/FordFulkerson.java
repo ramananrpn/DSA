ca793ac78f4e88da23ac23788a5c7b093b153c17
ramananrpn <ramananrpn@gmail.com>
2023-08-03 00:17:50 +0530

    FordFulkerson

---
+++ Algorithms/greedy/FordFulkerson.java
@@ -0,0 +1,9 @@
+package greedy;
+
+/*
+https://www.programiz.com/dsa/ford-fulkerson-algorithm
+https://www.scaler.com/topics/data-structures/ford-fulkerson-algorithm-for-maximum-flow-problem/
+https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/
+ */
+public class FordFulkerson {
+}
