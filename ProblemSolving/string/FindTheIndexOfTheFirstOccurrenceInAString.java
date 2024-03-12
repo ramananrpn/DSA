8a30a460acffb6ded50076aaaa789802e7971b8d
ramananrpn <ramananrpn@gmail.com>
2024-03-12 22:41:57 +0530

    FindTheIndexOfTheFirstOccurrenceInAString

--- ProblemSolving/string/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ ProblemSolving/string/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,5 +1,7 @@
 package string;
 
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
 /*
 Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 Example 1:
