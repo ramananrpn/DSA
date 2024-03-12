02c6d42ec8d75b0315ae90d92e49642f358c777f
ramananrpn <ramananrpn@gmail.com>
2024-03-12 22:22:48 +0530

    MergeTwoSortedLists

--- ProblemSolving/linkedlist/MergeTwoSortedLists.java
+++ ProblemSolving/linkedlist/MergeTwoSortedLists.java
@@ -67,5 +67,23 @@ public class MergeTwoSortedLists {
         }
 
         return head;
+
+//        /*
+//        ListNode head = new ListNode(-1);
+//        ListNode cur = head;
+//        while(list1 != null && list2 != null) {
+//            if(list1.val <= list2.val) {
+//                cur.next = list1;
+//                list1 = list1.next;
+//            } else {
+//                cur.next = list2;
+//                list2 = list2.next;
+//            }
+//            cur = cur.next;
+//        }
+//        cur.next = (list1 != null) ? list1 : list2;
+//
+//        return head.next;
+//        */
     }
 }
