1fb01be022510e0932d3eadbc0c0aacaa290d3ec
ramananrpn <ramananrpn@gmail.com>
2026-01-17 21:40:29 +0530

    SubarraySumEqualsK

---
+++ Patterns/prefixSum/SubarraySumEqualsK.java
@@ -0,0 +1,54 @@
+package prefixSum;
+
+// https://leetcode.com/problems/subarray-sum-equals-k/
+/*
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+
+
+Constraints:
+
+    1 <= nums.length <= 2 * 104
+    -1000 <= nums[i] <= 1000
+    -107 <= k <= 107
+ */
+
+
+// https://www.youtube.com/watch?v=fFVZt-6sgyo
+
+// tags : medium, array, hashmap, prefix_sum
+
+import java.util.HashMap;
+
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> prefixSum = new HashMap<>();
+        prefixSum.put(0, 1); // base case
+
+        int sum = 0, count = 0;
+
+        for(int num : nums) {
+            sum = sum+num;
+
+            if(prefixSum.containsKey(sum - k)) {
+                count += prefixSum.get(sum - k);
+            }
+            prefixSum.put(sum, prefixSum.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
