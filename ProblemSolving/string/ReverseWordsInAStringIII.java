25026b9d0148892aabe19198a63fee03ae3da321
ramananrpn <ramananrpn@gmail.com>
2026-06-27 22:28:17 +0530

    ReverseWordsInAStringIII

--- ProblemSolving/string/ReverseWordsInAStringIII.java
+++ ProblemSolving/string/ReverseWordsInAStringIII.java
@@ -42,3 +42,25 @@ public class ReverseWordsInAStringIII {
         return new String(str);
     }
 }
+
+
+
+
+// ----------------------------
+
+class ReverseWordsInAStringIIIWithSB {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            result.append(new StringBuilder(words[i]).reverse());
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+}
