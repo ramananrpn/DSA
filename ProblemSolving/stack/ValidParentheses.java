997f1a7040fc3766949b9d2339c35203ca0fdaf9
ramananrpn <ramananrpn@gmail.com>
2026-07-01 23:23:49 +0530

    ValidParentheses.java

--- ProblemSolving/stack/ValidParentheses.java
+++ ProblemSolving/stack/ValidParentheses.java
@@ -120,3 +120,78 @@ class ValidParenthesesUsingMapAndStack {
         return stack.isEmpty();
     }
 }
+
+// -----
+class ValidParenthesesUsingMapAndStackII {
+
+    // Hash table that takes care of the mappings.
+    private HashMap<Character, Character> mappings;
+
+    // Initialize hash map with mappings. This simply makes the code easier to read.
+    public ValidParenthesesUsingMapAndStackII() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+
+    public boolean isValid(String s) {
+
+        // Initialize a stack to be used in the algorithm.
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the current character is a closing bracket.
+            if (this.mappings.containsKey(c)) {
+
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(c);
+            }
+        }
+
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty();
+    }
+}
+
+
+// -------
+
+class ValidParenthesesUsingSimple {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()) {
+
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+
+                if(stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if(
+                        (ch == ')' && top != '(') ||
+                                (ch == '}' && top != '{') ||
+                                (ch == ']' && top != '[')
+                ) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
